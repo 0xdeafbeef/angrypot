fa04fd4d30cb9443b28991988a926e9625c1caad
0xd34b33f <0xd34b33f@users.noreply.github.com>
2020-06-19 22:58:49 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{Mutex, Arc};
+use thrussh::*;
+use thrussh::server::{Auth, Session};
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    
+    let mut config = thrussh::server::Config::default();
+    config.connection_timeout = Some(std::time::Duration::from_secs(3));
+    config.auth_rejection_time = std::time::Duration::from_secs(3);
+    
+    let config = Arc::new(config);
+    let sh = Server{
+        clients: Arc::new(Mutex::new(HashMap::new())),
+        id: 0
+    };
+    dbg!("running");
+    thrussh::server::run(config, "localhost:2222", sh).await
+}
+
+#[derive(Clone)]
+struct Server {
+    clients: Arc<Mutex<HashMap<(usize, ChannelId), thrussh::server::Handle>>>,
+    id: usize,
+}
+
+impl server::Server for Server {
+    type Handler = Self;
+    fn new(&mut self, _: Option<std::net::SocketAddr>) -> Self {
+        let s = self.clone();
+        self.id += 1;
+        s
+    }
+}
+
+impl server::Handler for Server {
+    type FutureAuth = futures::future::Ready<Result<(Self, server::Auth), anyhow::Error>>;
+    type FutureUnit = futures::future::Ready<Result<(Self, Session), anyhow::Error>>;
+    type FutureBool = futures::future::Ready<Result<(Self, Session, bool), anyhow::Error>>;
+    
+    fn finished_auth( self, auth: Auth) -> Self::FutureAuth {
+        futures::future::ready(Ok((self, auth)))
+    }
+    fn finished_bool(self, b: bool, s: Session) -> Self::FutureBool {
+        futures::future::ready(Ok((self, s, b)))
+    }
+    fn finished(self, s: Session) -> Self::FutureUnit {
+        futures::future::ready(Ok((self, s)))
+    }
+    fn auth_password(self, user: &str, password: &str) -> Self::FutureAuth {
+        println!("{} {}", user, password);
+        self.finished_auth(server::Auth::Accept)
+    }
+    
+    fn channel_open_session(self, channel: ChannelId, session: Session) -> Self::FutureUnit {
+        {
+            let mut clients = self.clients.lock().unwrap();
+            clients.insert((self.id, channel), session.handle());
+        }
+        self.finished(session)
+    }
 }
